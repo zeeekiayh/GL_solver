ee8e1f953d7136c4a915b7c8d8e3e354c8d3d573
zeeekiayh <60297297+zeeekiayh@users.noreply.github.com>
2021-07-20 14:49:43 -0600

    Merged changes compared to 'real-valued-...'

--- Dist_B_Phase.cpp
+++ Dist_B_Phase.cpp
@@ -3,10 +3,7 @@
 //  ================
 //  Global Variables
 //  ================
-    in_conditions c;
     VectorXcd guess, f;
-    SparseMatrix<complex<double>> L;
-    Matrix<vector<int>,3,3> var_mat;
 //  ================
 
 int main()
@@ -22,7 +19,7 @@ int main()
     Axx.typeL = string("Neumann");
     Axx.typeR = string("Neumann");
 
-    Axz.bB = 2.;         // a' = a(B)/b
+    Axz.bB = 0.;         // a = 0
     Axz.bT = pow(10,12); // a' = 0
     Axz.bL = pow(10,12); // a' = 0
     Axz.bR = pow(10,12); // a' = 0
@@ -49,10 +46,10 @@ int main()
     Azx.typeL = string("Neumann");
     Azx.typeR = string("Neumann");
 
-    Azz.bB = 0.;  // a = 0
+    Azz.bB = 0.;         // a = 0
     Azz.bT = pow(10,12); // a' = 0
-    Azz.bL = 1.;  // a(L) =  1
-    Azz.bR = -1.; // a(R) = -1
+    Azz.bL = 1.;         // a(L) =  1
+    Azz.bR = -1.;        // a(R) = -1
     Azz.typeB = string("Dirichlet");
     Azz.typeT = string("Neumann");
     Azz.typeL = string("Dirichlet");
@@ -66,12 +63,11 @@ int main()
     gls.BuildProblem(5,Axx,Axz,Ayy,Azx,Azz);
 
     // make a guess
-    int size = gls.getSolverMatrixSize();
+    int size = gls.getSolverMatrixSize(); // the number of columns in the matrix
     in_conditions c = gls.getConditions();
     int comp_length = size/5; // the length of each component in guess
-    int num;
-    
-    VectorXcd guess(size);
+    int num; // which OP component we're at
+    guess.resize(size);
     for (int i = 0; i < size; i++)
     {
         num = floor(i/comp_length);
@@ -93,7 +89,6 @@ int main()
             guess(i) = ((i-4*comp_length)%c.SIZEu)/2 ? 1. : -1.; // if on the left half, put +1, otherwise put -1
             if (i < 4*comp_length + c.SIZEu) guess(i) = Azz.bB;  // set the bottom to its boundary value
             break;
-        
         default:
             break;
         }
