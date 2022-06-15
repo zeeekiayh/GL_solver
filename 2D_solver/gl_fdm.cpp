cc91b186b84aa420c60a324ab48622ad19b1aa0d
izek <hor19019@byui.edu>
2022-06-15 10:46:32 -0600

    check for unknown "file_name"

--- 2D_solver/gl_fdm.cpp
+++ 2D_solver/gl_fdm.cpp
@@ -49,11 +49,15 @@ int main(int argc, char** argv)
 	if (argc == 3 && *(argv[2]) == 'c') { // if it is for a cylindrical system
 		pSC = new Cylindrical ( Nop, cond.SIZEu, cond.SIZEv, cond.STEP, eta_BC );
 		if (Nop == 5) {
-			if (file_name == string("conditions5c.txt"))         pSC->initialOPguess_Cylindrical_simple5(eta_BC, OPvector, no_update);
-			if (file_name == string("conditions5c_AzzFlip.txt")) pSC->initialOPguess_Cylindrical_AzzFlip(eta_BC, OPvector, no_update); // TODO: make file, "conditions5c_AzzFlip.txt"
-			if (file_name == string("conditions5c_bubble.txt"))  pSC->initialOPguess_Cylindrical_bubble (eta_BC, OPvector, no_update);
+			if (file_name == string("conditions5c.txt"))              pSC->initialOPguess_Cylindrical_simple5(eta_BC, OPvector, no_update);
+			else if (file_name == string("conditions5c_AzzFlip.txt")) pSC->initialOPguess_Cylindrical_AzzFlip(eta_BC, OPvector, no_update); // TODO: make file, "conditions5c_AzzFlip.txt"
+			else if (file_name == string("conditions5c_bubble.txt"))  pSC->initialOPguess_Cylindrical_bubble (eta_BC, OPvector, no_update);
+			else {
+				cout << "Unknown file_name. Exiting..." << endl;
+				delete pSC;
+				return 1;
+			}
 		} else if (Nop == 3) {
-			// code this next!
 			if (file_name == string("conditions3c.txt")) pSC->initialOPguess_Cylindrical_simple3(eta_BC, OPvector, no_update);
 			else {cout << "WARNING: you either added the optional argument '[c]' on accident, or passed in the wrong file name for a cylindrical system." << endl; return 1;}
 		}
@@ -66,9 +70,14 @@ int main(int argc, char** argv)
 	} else if (Nop == 5) {
 		pSC = new FiveCompHe3( Nop, cond.SIZEu, cond.SIZEv, cond.STEP );
 		// ---- set the OP vector to a good guess based on BC's ----
-		if (file_name == string("conditions5.txt"))         pSC->initialOPguess             (eta_BC, OPvector, no_update);
-		if (file_name == string("conditions5_AyyFlip.txt")) pSC->initGuessWithCircularDomain(eta_BC, OPvector, no_update);
-		if (file_name == string("conditions5_wall.txt"))    pSC->initOPguess_special(cond, eta_BC, OPvector, no_update); // Anton's version
+		if (file_name == string("conditions5.txt"))              pSC->initialOPguess             (eta_BC, OPvector, no_update);
+		else if (file_name == string("conditions5_AyyFlip.txt")) pSC->initGuessWithCircularDomain(eta_BC, OPvector, no_update);
+		else if (file_name == string("conditions5_wall.txt"))    pSC->initOPguess_special(cond, eta_BC, OPvector, no_update); // Anton's version
+		else {
+			cout << "Unknown file_name. Exiting..." << endl;
+			delete pSC;
+			return 1;
+		}
 	} else if (Nop == 1) {
 		pSC = new OneCompSC( Nop, cond.SIZEu, cond.SIZEv, cond.STEP );
 	} else {
@@ -81,7 +90,7 @@ int main(int argc, char** argv)
 
 	// if (debug) { // write the initial guess to file, for debugging
 	// 	// pSC->WriteToFile(OPvector, "initGuess"+to_string(Nop)+".txt", 1);
-	// 	pSC->WriteAllToFile(OPvector, freeEb, freeEg, "initGuess_output_OP"+to_string(Nop)+".txt");
+		pSC->WriteAllToFile(OPvector, freeEb, freeEg, "initGuess_output_OP"+to_string(Nop)+".txt");
 	// }
 
 	// ===============================================================================================================
@@ -128,6 +137,7 @@ int main(int argc, char** argv)
 	// ===============================================================================================================
 
 	cout << "solving system..." << endl;
+	// rhsBC = T_vector::Zero(VectSize);
 	Solver(OPvector, M, rhsBC, cond, no_update, pSC); // solve the system setup above
 	cout << "solved!" << endl;
 
